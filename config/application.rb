7a75ca6ff945fc40022a31b2b6c7b5141b99c4f6
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2014-10-20 22:56:07 +0300

    Add styles for Device controllers

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,12 @@ module Nevermissyourmovie
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.to_prepare do
+      [
+        Devise::SessionsController,
+        Devise::RegistrationsController,
+        Devise::PasswordsController
+      ].each { |c| c.layout 'sessions' }
+    end
   end
 end
