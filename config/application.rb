32b73d6a660d8ff4080ebcb81cf9e5de068e6774
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2014-10-24 23:15:06 +0300

    Configure rails generators

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,18 @@ module Nevermissyourmovie
         Devise::PasswordsController
       ].each { |c| c.layout 'sessions' }
     end
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: true
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+
+      g.assets false
+    end
   end
 end
