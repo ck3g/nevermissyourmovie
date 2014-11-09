e4098d6bb6b67121cf8f5bdbffb7fd2e6e2577d8
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2014-11-09 09:10:04 +0200

    Can view movies list

--- spec/features/movies/movies_list_spec.rb
+++ spec/features/movies/movies_list_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 feature 'Movies list' do
   scenario 'can view movies list' do
+    create :movie, title: 'The Big Bang Theory'
     visit movies_path
 
     expect(page).to have_content "Movies"
+    expect(page).to have_content 'The Big Bang Theory'
   end
 end
