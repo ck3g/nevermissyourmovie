bf0b1fdd9222db14dff5c10ce5a34a865cf7ca95
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2014-11-09 09:14:30 +0200

    Navigate to movies list

--- spec/features/movies/movies_list_spec.rb
+++ spec/features/movies/movies_list_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 feature 'Movies list' do
   scenario 'can view movies list' do
+    create :user_example_com
     create :movie, title: 'The Big Bang Theory'
-    visit movies_path
+
+    sign_in_as 'user@example.com'
+    within 'div.navbar' do
+      click_link 'Movies'
+    end
 
     expect(page).to have_content "Movies"
     expect(page).to have_content 'The Big Bang Theory'
